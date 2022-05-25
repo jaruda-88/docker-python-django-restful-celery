dae7608912fbd292e3235bd3bd179ac8bedae680
jeongmin.lee <jaruda88@gmail.com>
2022-05-25 17:32:19 +0900

    working

--- jaruda_back/todos/models.py
+++ jaruda_back/todos/models.py
@@ -1,11 +1,22 @@
 from pyexpat import model
+from turtle import Turtle
 from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
+    dt_id = models.ForeignKey("DTType", related_name="DTType", db_column="dt_id", blank=True, null=True)
     task = models.CharField(max_length=100)
+    expire_at = models.DateTimeField(default=None, blank=True, null=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
         return self.task
+
+
+class DTType(models.Model):
+    name = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.name
